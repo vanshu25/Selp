c02e32bc79ad3ded1a10548e1b5cc0fbf278a315
Vanshika <vanshikanamdev.nakur@gmail.com>
2021-09-30 11:39:22 +0530

    updated

--- mentalHealthPred.py
+++ mentalHealthPred.py
@@ -13,9 +13,19 @@ y = le.fit_transform(y)
 
 
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
-knn = KNeighborsClassifier(n_neighbors=12)
-sv = knn.fit(X, y)
+from sklearn import svm
+import sklearn.model_selection as model_selection
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import f1_score
 
-pickle.dump(sv, open('MHP.pkl', 'wb'))
+
+poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, y_train)
+poly_pred = poly.predict(X_test)
+
+# poly_accuracy = accuracy_score(y_test, poly_pred)
+# poly_f1 = f1_score(y_test, poly_pred, average='weighted')
+
+# print('Accuracy (Polynomial Kernel): ', "%.2f" % (poly_accuracy*100))
+# print('F1 (Polynomial Kernel): ', "%.2f" % (poly_f1*100))
+pickle.dump(poly, open('MHP.pkl', 'wb'))
